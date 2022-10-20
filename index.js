b12f12f0b71a52a8468a66325e9266ff818f028c
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2022-10-20 08:29:22 -0500

    Final clase jueves

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const container = document.getElementById('container');
+const nameInput = document.getElementById('nameInput');
+const idInput = document.getElementById('idInput');
+const programInput = document.getElementById('programInput');
+const addBtn = document.getElementById('addBtn');
 
+//Los datos a representar
+const data = [];
 
 function generate(){
     for(let i=0 ; i<20 ; i++){
         let est = new Estudiante(`A000${i}`,`Name${i}`,`DMI`);
-        est.render(container);
+        est.delete = function(){
+            let index = data.indexOf(est);
+            data.splice(index, 1);
+            showData();
+        }
+        data.push(est);
+    }
+}
+
+function showData(){
+    container.innerHTML = "";
+    for(let i=0 ; i<data.length ; i++){
+        data[i].render(container);
     }
 }
 
 //Usar la función
-generate();
+//generate();
+showData();
+
+function addCard(){
+    let name = nameInput.value;
+    let id = idInput.value;
+    let program = programInput.value;
+    let estudiante = new Estudiante(id, name, program);
+    estudiante.delete = function(){
+        let index = data.indexOf(estudiante);
+        data.splice(index, 1);
+        showData();
+    }
+    data.push(estudiante);
+    showData();
+}
+
+addBtn.addEventListener('click', addCard);
--- model/Estudiante.js
+++ model/Estudiante.js
@@ -4,13 +4,14 @@ class Estudiante{
         this.id = id;
         this.name = name;
         this.program = program;
+        this.delete = null;
     }
     render(container){
         let content = `<h3>${this.name}</h3>
                         <b>${this.id}</b>
                         <small>${this.program}</small>
                         <button id="show${this.id}">SHOW</button>
-                        <button>DELETE</button>`;
+                        <button id="delete${this.id}">DELETE</button>`;
         let card = document.createElement('div');
         //let cardString = '<div></div>';
         //Agregar contenido a nivel de string
@@ -18,8 +19,11 @@ class Estudiante{
         //Agregar la card al contenedor
         //Agregar contenido pero a nivel de elementos
         container.appendChild(card);
-        let showBtn = document.getElementById(`show${this.id}`);   
+        let showBtn = document.getElementById(`show${this.id}`);
         showBtn.addEventListener('click', this.show.bind(this));
+
+        let deleteBtn = document.getElementById(`delete${this.id}`);
+        deleteBtn.addEventListener('click', this.delete);
    }
 
    show(){
